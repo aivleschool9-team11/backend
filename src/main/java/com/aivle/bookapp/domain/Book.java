7a66caea3805e2a7e92753128d705da8b336f5e9
niiryoo <yoojiin0328@naver.com>
2026-06-24 13:46:25 +0900

    fix: Book.java VectorConverter 불필요한 import 제거

--- src/main/java/com/aivle/bookapp/domain/Book.java
+++ src/main/java/com/aivle/bookapp/domain/Book.java
@@ -1,6 +1,5 @@
 package com.aivle.bookapp.domain;
 
-import com.aivle.bookapp.converter.VectorConverter;
 import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
